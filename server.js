0ffb9df3cc43145e8689476f9dddaf702954320b
Shubham <shubhamraj2468sr@gmail.com>
2026-06-16 05:15:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,20 @@ app.post("/ask", async (req, res) => {
 
         const question = req.body.question;
 
-        const response = await fetch(
-"https://bmw-ai-backend.onrender.com/ask",
+       const response = await fetch(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
 {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    question: systemPrompt + "\n\n" + finalQuestion
-  })
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        contents: [{
+            parts: [{
+                text: question
+            }]
+        }]
+    })
 }
 );
         const data = await response.json();
