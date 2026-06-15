7eae168f9ca6b3421771eda2cfbf23e96f1e0fb9
Shubham <shubhamraj2468sr@gmail.com>
2026-06-16 05:16:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,44 +9,48 @@ app.use(express.json());
 
 app.post("/ask", async (req, res) => {
 
-    try {
+try {
 
-        const question = req.body.question;
+const question = req.body.question;
 
-       const response = await fetch(
+const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
 {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        contents: [{
-            parts: [{
-                text: question
-            }]
-        }]
-    })
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+contents: [{
+parts: [{
+text: question
+}]
+}]
+})
 }
 );
-        const data = await response.json();
 
-        res.json(data);
+const data = await response.json();
 
-    } catch (err) {
+console.log(data);
 
-        console.error(err);
+res.json(data);
 
-        res.status(500).json({
-            error: "Server Error"
-        });
+}
+catch(err){
 
-    }
+console.error(err);
+
+res.status(500).json({
+error: "Server Error"
+});
+
+}
 
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`BMW AI Backend Running On Port ${PORT}`);
+console.log(`BMW AI Backend Running On Port ${PORT}`);
 });
